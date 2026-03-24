18dcc39d90ce6b384e4656c5be386957d161a06e
vaz16505-ai <vaz16505@gmail.com>
2026-03-24 16:12:50 -0300

    Add files via upload

---
+++ Jogo de cartas/src/Resultado.java
@@ -0,0 +1,11 @@
+public class Resultado {
+    long tempo;
+    int comparacoes;
+    int trocas;
+
+    public Resultado(long tempo, int comparacoes, int trocas) {
+        this.tempo = tempo;
+        this.comparacoes = comparacoes;
+        this.trocas = trocas;
+    }
+}
